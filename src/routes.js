821a9d2e83e4a0206ea259e46e2315fde1351341
unknown <alber725@gmail.com>
2020-05-07 13:08:24 -0300

    Tabela project criada e populada. Rota get criada

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,15 @@ const express = require('express')
 const routes = express.Router()
 
 const UserController = require('./controllers/UserController')
+const ProjectController = require('./controllers/ProjectController')
 
+// Users
 routes.get('/users', UserController.index)
 routes.post('/users', UserController.create)
 routes.put('/users/:id', UserController.update)
 routes.delete('/users/:id', UserController.delete)
+
+// Projects
+routes.get('/projects', ProjectController.index)
 
 module.exports = routes
